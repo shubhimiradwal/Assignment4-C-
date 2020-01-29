b4d728f372f3c7abd22e58ce6ea6469540b8b6b3
shubhimiradwal <40629901+shubhimiradwal@users.noreply.github.com>
2020-01-29 16:56:56 +0530

    Add files via upload

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> a1 = new List<string>() { "Boat", "house", "cat", "river", "cupboard" };
+            for (int i = 0; i < a1.Count; i++)
+            {
+                string a2 = a1[i] + "s";
+                Console.WriteLine(a2);
+
+            }
+            a1[1] = a1[1].Replace("house","home");
+            Console.WriteLine(a1[1]);
+            a1.Add("shubhii");
+            a1.Add("purvi");
+
+            for (int i= 0;i< a1.Count;i++)
+            {
+                if (a1[i].Length == 7)
+                {
+                    Console.WriteLine(a1[i]);
+                }
+            }
+            a1.Reverse();
+            foreach (string value in a1)
+            {
+                Console.WriteLine(value);
+            }
+
+            a1.Sort();
+            
+            foreach(string value in a1)
+            {
+                Console.WriteLine(value);
+            }
+
+            
+            
+
+        }
+    }
+}
